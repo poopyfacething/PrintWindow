dc52266ae640c565b7a2093b5ab5d84ba686a2b4
poopyfacething <steveishe598@gmail.com>
2025-04-01 10:55:45 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,49 +1,32 @@
-const { Client } = require('chrome-remote-interface');
 const express = require('express');
 const path = require('path');
+const CDP = require('chrome-remote-interface');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Start the server
-app.get('/', (req, res) =&gt; {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(PORT, () =&gt; {
+app.listen(PORT, async () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 
-    // Run the script to print all tabs
-    (async () =&gt; {
-        const client = await Client.create();
-        await client.connectToTarget('localhost:8080');
-
-        const { Network, Page, DOM, Runtime} = client.Target.createTargetSpec('', {useSingleRPCHandler: true});
-
-        await Promise.all([Network.enable(), Page.enable(), DOM.enable(), Runtime.enable()]);
-
-        const { Tab } = client.Target.addTargetSpec('', {useSingleRPCHandler: true});
-        const tabId = await Page.getAll().then(pages => pages[0].id);
-
-        const { Tab.get, Tab.sendCommand } = client.Target.getTarget(tabId);
-
-        await Tab.get(tabId, ['tabs']);
-
-        const { tabs } = await Tab.get(tabId);
-
-        console.log(tabs);
-
-        // Send the tabs data to the client
-        client.on('Target.Tab.added', (tab) =&gt; {
-            client.send('Runtime.evaluate', {
-                expression: `window.tabs = ${JSON.stringify(tabs)};`,
-                runInContext: tab.id,
-            });
-        });
+    try {
+        const client = await CDP({ host: 'localhost', port: 9222 });
+        const { Network, Page } = client;
+        
+        await Promise.all([Network.enable(), Page.enable()]);
+        
+        const tabs = await CDP.List();
+        console.log('Open tabs:', tabs);
 
         await client.close();
-    })();
+    } catch (error) {
+        console.error('Error connecting to Chrome DevTools:', error);
+    }
 });
